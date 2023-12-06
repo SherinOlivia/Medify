c0b1338ccaad2e0dcf4751b929b719bfd8fa9371
Sherin Olivia <SOChronicle@gmail.com>
2023-12-06 21:32:10 +0700

    chore: updated registerUser import path

--- src/router/authRouter.ts
+++ src/router/authRouter.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { registerUser } from '.././controller/registerController';
+import { registerUser } from '../controller/registerController';
 // import { loginUser } from '.././controller/loginController';
 
 const authrouter = express.Router()
