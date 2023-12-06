ca862e2855b6b15604de90e61894791d9010807a
Sherin Olivia <SOChronicle@gmail.com>
2023-12-06 20:47:30 +0700

    chore: added error handling

---
+++ src/controller/errorHandling.ts
@@ -0,0 +1,13 @@
+export const errorHandling = function (data: any, error: any) {
+    if (error) {
+        return {
+            success: false,
+            error: error
+        }
+    }
+
+    return {
+        success: true,
+        data: data
+    }
+}
