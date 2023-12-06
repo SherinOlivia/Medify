7f291fb02f427e6b90631efd0588cfa486133ed2
Sherin Olivia <SOChronicle@gmail.com>
2023-12-06 21:19:59 +0700

    chore: activated router

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 // import mongoMiddleware from './middleware/mongomiddleware'
-// import router from './router/mainRouter';
+import router from './router/mainRouter';
 import 'dotenv/config'
 
 const port = process.env.PORT;
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.json())
 
 // app.use(mongoMiddleware)
-// app.use(router)
+app.use(router)
 
 app.listen(port, () => {
     console.log(`Server is running on port:${port}`)
