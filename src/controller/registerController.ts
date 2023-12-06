36a65f569b55e557e97a5cb7b34c86c0dbb95b4b
Sherin Olivia <SOChronicle@gmail.com>
2023-12-06 15:48:54 +0700

    chore: changed services name to controller

---
+++ src/controller/registerController.ts
@@ -0,0 +1,5 @@
+// import { Request, Response, NextFunction } from "express";
+// import bcrypt from 'bcrypt'
+// import jwt, { Secret } from 'jsonwebtoken'
+// import JWT_TOKEN from '../config/jwtConfig'
+// import { error } from "console";
