e4f146ee88511c067ad7f0a981c4bce947b5d6c2
Sherin Olivia <SOChronicle@gmail.com>
2023-12-07 14:53:55 +0700

    chore: added a setup for closing the connection after each request

--- src/middleware/mongoMiddleware.ts
+++ src/middleware/mongoMiddleware.ts
@@ -1,24 +1,35 @@
-import {Request, Response, NextFunction} from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { Db, MongoClient } from 'mongodb';
+import 'dotenv/config';
 
-const uri = 'mongodb+srv://admin:admin@cluster0.kip1euw.mongodb.net/?retryWrites=true&w=majority'
+const uri = process.env.URI!;
 
-const mongoMiddleware = async (req: any, res: Response, next : NextFunction) => {
+const mongoMiddleware = async (req: any, res: Response, next: NextFunction) => {
+    let client: MongoClient | null = null;
+
     try {
-        const mongoClient = await new MongoClient(uri).connect();
-        const db: Db = mongoClient.db('chatApp');
-    
-        req.db = db
-        if(db){
+        console.log("MongoDB Connection Test..!");
+        client = await new MongoClient(uri).connect();
+        const db: Db = client.db('chatApp');
+        req.db = db;
+
+        if (db) {
             console.log("MongoDB Connection Succeed..!");
             next();
         } else {
             console.log("MongoDB Connection Failed...");
         }
-        
+
     } catch (error) {
         console.log("MongoDB Connection Failed..:", error);
+    } finally {
+        res.on('finish', async () => {
+            if (client) {
+                await client.close();
+                console.log("MongoDB Connection Closed..!");
+            }
+        });
     }
-  };
-  
+};
+
 export default mongoMiddleware;
