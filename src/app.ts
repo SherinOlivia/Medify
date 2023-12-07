5494a80ecfebc7bdc8cb8b68eb29e93c523e6ccd
Sherin Olivia <SOChronicle@gmail.com>
2023-12-07 15:06:33 +0700

    fix: fixed super admin function

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoMiddleware from './middleware/mongoMiddleware'
 import router from './router/mainRouter';
 import 'dotenv/config'
+import insertAdmin from './config/superAdmin';
 
 const port = process.env.PORT;
 const app = express()
@@ -10,6 +11,12 @@ app.use(express.json())
 
 app.use(mongoMiddleware)
 app.use(router)
+
+// super admin account:
+app.post('/setupAdmin', async (req, res) => {
+  await insertAdmin(req);
+  res.status(200).send('Admin setup complete');
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port:${port}`)
