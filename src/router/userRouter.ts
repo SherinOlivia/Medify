6ca73676712a5489223cbc770dfb62bfcb2849ed
Sherin Olivia <SOChronicle@gmail.com>
2023-12-13 19:02:13 +0700

    feat: created update user data function

--- src/router/userRouter.ts
+++ src/router/userRouter.ts
@@ -1,11 +1,12 @@
 import express from 'express'
 import authorMiddleware from '../middleware/authorizationMiddleware';
-import { getUserProfile, getUserProfileByAdmin, getUsersList } from '../controller/userController';
+import { getUserProfile, getUserProfileByAdmin, getUsersList, updateUser } from '../controller/userController';
 
 const userRouter = express.Router()
 
 userRouter.get('/profile', getUserProfile);
 userRouter.get('/profile/:userId', authorMiddleware(['staff','admin']), getUserProfileByAdmin);
 userRouter.get('/list', authorMiddleware(['staff','admin']), getUsersList)
+userRouter.put('/update/:userId', updateUser);
 
 export default userRouter;
