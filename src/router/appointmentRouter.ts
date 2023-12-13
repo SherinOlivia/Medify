bf2960662dcc8f4b9ee38b550c440bc8e36b1c84
Sherin Olivia <SOChronicle@gmail.com>
2023-12-13 23:22:39 +0700

    fix: fixed 'user' -> 'patient' authorization

--- src/router/appointmentRouter.ts
+++ src/router/appointmentRouter.ts
@@ -4,9 +4,9 @@ import authorMiddleware from '../middleware/authorizationMiddleware';
 const appointmentRouter = express.Router()
 
 appointmentRouter.get('/list', getAppointmentList)
-appointmentRouter.post('/new', authorMiddleware(['user']), createAppointment);
+appointmentRouter.post('/new', authorMiddleware(['patient']), createAppointment);
 appointmentRouter.put('/update/:appointmentId', updateAppointment);
-appointmentRouter.patch('/cancel/:appointmentId', authorMiddleware(['user']), cancelAppointment);
+appointmentRouter.patch('/cancel/:appointmentId', authorMiddleware(['patient']), cancelAppointment);
 appointmentRouter.patch('/update/:appointmentId', authorMiddleware(['medical_admin','staff','admin']), updateAppointmentStatus);
 
 export default appointmentRouter;
