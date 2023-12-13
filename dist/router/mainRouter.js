95f707c7a1cae7ef930d1a035636059f4d3b42c8
Sherin Olivia <SOChronicle@gmail.com>
2023-12-13 19:05:02 +0700

    feat: activated appointment route

--- dist/router/mainRouter.js
+++ dist/router/mainRouter.js
@@ -5,11 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const authenticationMiddleware_1 = __importDefault(require("../middleware/authenticationMiddleware"));
-const authRouter_1 = __importDefault(require("./authRouter"));
-const adminAuthRouter_1 = __importDefault(require("./adminAuthRouter"));
-const userRouter_1 = __importDefault(require("./userRouter"));
-const medicalPersonnelRouter_1 = __importDefault(require("./medicalPersonnelRouter"));
-const medicalFacilityRouter_1 = __importDefault(require("./medicalFacilityRouter"));
+const router_1 = require("../router");
 const router = express_1.default.Router();
 router.get("/", function (req, res) {
     res.status(200).json({
@@ -17,9 +13,10 @@ router.get("/", function (req, res) {
         message: "Hello, this is Medify! Group [e]'s Final Project!"
     });
 });
-router.use('/api/v1/auth', authRouter_1.default);
-router.use('/api/v1/admin/auth', authenticationMiddleware_1.default, adminAuthRouter_1.default);
-router.use('/api/v1/user', authenticationMiddleware_1.default, userRouter_1.default);
-router.use('/api/v1/medic', authenticationMiddleware_1.default, medicalPersonnelRouter_1.default);
-router.use('/api/v1/facility', authenticationMiddleware_1.default, medicalFacilityRouter_1.default);
+router.use('/api/v1/auth', router_1.authRouter);
+router.use('/api/v1/admin/auth', authenticationMiddleware_1.default, router_1.adminAuthRouter);
+router.use('/api/v1/user', authenticationMiddleware_1.default, router_1.userRouter);
+router.use('/api/v1/medic', authenticationMiddleware_1.default, router_1.medicalPersonnelRouter);
+router.use('/api/v1/facility', authenticationMiddleware_1.default, router_1.medicalFacilityRouter);
+router.use('/api/v1/appointment', authenticationMiddleware_1.default, router_1.appointmentRouter);
 exports.default = router;
