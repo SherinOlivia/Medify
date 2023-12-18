4c120e3ab1da7e4fa6253f55d7e62a4f1f0739a7
Sherin Olivia <SOChronicle@gmail.com>
2023-12-19 01:16:46 +0700

    chore: compiled middlewares as one 'appMiddleware'

---
+++ src/middleware/index.ts
@@ -0,0 +1,22 @@
+import { Application } from "express";
+import cookieMiddleware from "./cookieMiddleware";
+import expressMiddleware from "./expressMiddleware";
+import helmetMiddleware from "./helmetMiddleware";
+import requestIdMiddleware from "./requestMiddleware";
+import morganMiddleware from "./morganMiddleware";
+import corsMiddleware from "./corsMiddleware";
+import escapeHtmlMiddleware from "./escapeHtmlMiddleware";
+import mongoMiddleware from "./mongoMiddleware";
+
+const appMiddleware = (app: Application) => {
+    app.use(mongoMiddleware);
+    app.use(requestIdMiddleware);
+    app.use(escapeHtmlMiddleware);
+    morganMiddleware(app);
+    helmetMiddleware(app);
+    expressMiddleware(app);
+    cookieMiddleware(app);
+    corsMiddleware(app);
+}
+
+export default appMiddleware
